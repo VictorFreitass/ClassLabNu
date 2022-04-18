2bf9be242f2a80ca93a521fe1335e22be330e228
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2022-04-18 11:48:25 -0300

    Cliente

--- ComercialSys91/FrmCliente.Designer.cs
+++ ComercialSys91/FrmCliente.Designer.cs
@@ -37,8 +37,21 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnInserir = new System.Windows.Forms.Button();
-            this.lstClientes = new System.Windows.Forms.ListBox();
             this.btnListar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.txtDesativar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkAtivo = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpDataCad = new System.Windows.Forms.DateTimePicker();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.clnIdCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnCpf = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnEmail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnAtivo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // txtId
@@ -52,7 +65,7 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(119, 139);
+            this.txtNome.Location = new System.Drawing.Point(306, 110);
             this.txtNome.MaxLength = 60;
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(190, 20);
@@ -61,7 +74,7 @@
             // 
             // txtCpf
             // 
-            this.txtCpf.Location = new System.Drawing.Point(119, 165);
+            this.txtCpf.Location = new System.Drawing.Point(119, 149);
             this.txtCpf.MaxLength = 11;
             this.txtCpf.Name = "txtCpf";
             this.txtCpf.Size = new System.Drawing.Size(100, 20);
@@ -70,7 +83,7 @@
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(119, 191);
+            this.txtEmail.Location = new System.Drawing.Point(306, 149);
             this.txtEmail.MaxLength = 60;
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(190, 20);
@@ -89,7 +102,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 142);
+            this.label2.Location = new System.Drawing.Point(265, 113);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -98,7 +111,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(78, 168);
+            this.label3.Location = new System.Drawing.Point(78, 152);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 6;
@@ -108,7 +121,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(78, 191);
+            this.label4.Location = new System.Drawing.Point(265, 149);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 7;
@@ -119,29 +132,19 @@
             this.btnInserir.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnInserir.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInserir.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.btnInserir.Location = new System.Drawing.Point(119, 241);
+            this.btnInserir.Location = new System.Drawing.Point(81, 227);
             this.btnInserir.Name = "btnInserir";
             this.btnInserir.Size = new System.Drawing.Size(75, 23);
             this.btnInserir.TabIndex = 8;
-            this.btnInserir.Text = "Inserir";
+            this.btnInserir.Text = "&Inserir";
             this.btnInserir.UseVisualStyleBackColor = false;
             this.btnInserir.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // lstClientes
-            // 
-            this.lstClientes.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lstClientes.FormattingEnabled = true;
-            this.lstClientes.Location = new System.Drawing.Point(372, 103);
-            this.lstClientes.Name = "lstClientes";
-            this.lstClientes.Size = new System.Drawing.Size(190, 108);
-            this.lstClientes.TabIndex = 9;
-            this.lstClientes.SelectedIndexChanged += new System.EventHandler(this.lstClientes_SelectedIndexChanged);
             // 
             // btnListar
             // 
             this.btnListar.BackColor = System.Drawing.SystemColors.Window;
             this.btnListar.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.btnListar.Location = new System.Drawing.Point(433, 241);
+            this.btnListar.Location = new System.Drawing.Point(320, 227);
             this.btnListar.Name = "btnListar";
             this.btnListar.Size = new System.Drawing.Size(75, 23);
             this.btnListar.TabIndex = 10;
@@ -149,14 +152,143 @@
             this.btnListar.UseVisualStyleBackColor = false;
             this.btnListar.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Enabled = false;
+            this.btnAlterar.Location = new System.Drawing.Point(186, 227);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 11;
+            this.btnAlterar.Text = "&Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // txtDesativar
+            // 
+            this.txtDesativar.Location = new System.Drawing.Point(421, 227);
+            this.txtDesativar.Name = "txtDesativar";
+            this.txtDesativar.Size = new System.Drawing.Size(75, 23);
+            this.txtDesativar.TabIndex = 12;
+            this.txtDesativar.Text = "&Desativar";
+            this.txtDesativar.UseVisualStyleBackColor = true;
+            this.txtDesativar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Status";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // chkAtivo
+            // 
+            this.chkAtivo.AutoSize = true;
+            this.chkAtivo.Location = new System.Drawing.Point(119, 184);
+            this.chkAtivo.Name = "chkAtivo";
+            this.chkAtivo.Size = new System.Drawing.Size(50, 17);
+            this.chkAtivo.TabIndex = 14;
+            this.chkAtivo.Text = "Ativo";
+            this.chkAtivo.UseVisualStyleBackColor = true;
+            this.chkAtivo.CheckedChanged += new System.EventHandler(this.chkAtivo_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Data de Cadastro";
+            // 
+            // dtpDataCad
+            // 
+            this.dtpDataCad.Location = new System.Drawing.Point(306, 178);
+            this.dtpDataCad.Name = "dtpDataCad";
+            this.dtpDataCad.Size = new System.Drawing.Size(212, 20);
+            this.dtpDataCad.TabIndex = 16;
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.AllowUserToAddRows = false;
+            this.dgvClientes.AllowUserToDeleteRows = false;
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clnIdCli,
+            this.clnNome,
+            this.clnCpf,
+            this.clnEmail,
+            this.clnAtivo});
+            this.dgvClientes.Location = new System.Drawing.Point(81, 272);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.ReadOnly = true;
+            this.dgvClientes.RowHeadersVisible = false;
+            this.dgvClientes.Size = new System.Drawing.Size(437, 167);
+            this.dgvClientes.TabIndex = 17;
+            this.dgvClientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellContentClick);
+            // 
+            // clnIdCli
+            // 
+            this.clnIdCli.Frozen = true;
+            this.clnIdCli.HeaderText = "Id";
+            this.clnIdCli.Name = "clnIdCli";
+            this.clnIdCli.ReadOnly = true;
+            // 
+            // clnNome
+            // 
+            this.clnNome.Frozen = true;
+            this.clnNome.HeaderText = "Nome";
+            this.clnNome.Name = "clnNome";
+            this.clnNome.ReadOnly = true;
+            // 
+            // clnCpf
+            // 
+            this.clnCpf.Frozen = true;
+            this.clnCpf.HeaderText = "CPF";
+            this.clnCpf.Name = "clnCpf";
+            this.clnCpf.ReadOnly = true;
+            // 
+            // clnEmail
+            // 
+            this.clnEmail.Frozen = true;
+            this.clnEmail.HeaderText = "Email";
+            this.clnEmail.Name = "clnEmail";
+            this.clnEmail.ReadOnly = true;
+            // 
+            // clnAtivo
+            // 
+            this.clnAtivo.Frozen = true;
+            this.clnAtivo.HeaderText = "Ativo";
+            this.clnAtivo.Name = "clnAtivo";
+            this.clnAtivo.ReadOnly = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(163, 113);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(39, 20);
+            this.btnBuscar.TabIndex = 18;
+            this.btnBuscar.Text = "...";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // FrmCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(856, 626);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.dtpDataCad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chkAtivo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDesativar);
+            this.Controls.Add(this.btnAlterar);
             this.Controls.Add(this.btnListar);
-            this.Controls.Add(this.lstClientes);
             this.Controls.Add(this.btnInserir);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -169,6 +301,7 @@
             this.Name = "FrmCliente";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,8 +318,20 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnInserir;
-        private System.Windows.Forms.ListBox lstClientes;
         private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button txtDesativar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkAtivo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtpDataCad;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnIdCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnCpf;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnEmail;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clnAtivo;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
 
